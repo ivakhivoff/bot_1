14c3ee63c7941fa023a561825f47fe5ed3b5db5c
Мєнти нам не кєнти <81561715+ivakhivoff@users.noreply.github.com>
2026-04-03 13:58:35 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import json
+from pathlib import Path
+
+# from dotenv import load_dotenv
+import os
+
+# load_dotenv()/
+API_ID = 17658310
+API_HASH = "4531153c8ae00fe8458eeaeb45e057f9"
+BOT_TOKEN = "5983258128:AAERc_WRWWtEFZpoWxJHsUZc_Fk3qbKQx7M"
+ADMINS = [858979699, 301679635, 1615874595]
